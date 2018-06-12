1ced6f47f3c258c9727ee895994ea01f05c0a7ae
Drew Miley <drew.miley@opencastsoftware.com>
2018-06-12 17:16:56 +0100

    Adds cors

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const Song = require('./app/models/song');
 
 // Configure app
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const bodyParser = require('body-parser');
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 const port = process.env.PORT || 8080;
